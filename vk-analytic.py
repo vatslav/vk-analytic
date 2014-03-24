02541a5e8e73844f7acef7ef8b8fb4a6305eba4c
thundershark <thundershark@yandex.ru>
2014-03-24 16:20:33 +0400

    vk api get time + get credentrials for vk from file

--- vk-analytic.py
+++ vk-analytic.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 __author__ = 'salamander'
-key = 'TcgKndPB7ZDSiBkqZWw0'
-id = '4263392'
+
+import vk_auth
+import vkontakte
+
+def getCredent(file):
+    '''
+    @type param: file
+    '''
+    f = open(file,'r')
+    return [lines.strip() for lines in f]
+
+
+
+def main():
+    login, pas, appid, appkey = getCredent('credentials.txt')
+
+    (token,user_id) = vk_auth.auth(login, pas, appid, appkey)
+    vk = vkontakte.API(token=token)
+    print "Hello vk API , server time is ",vk.getServerTime()
+    return 0
+
+if __name__ == '__main__':
+    pass
+    main()
