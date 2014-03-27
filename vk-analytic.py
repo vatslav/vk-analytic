71e253f2e03d6ef9e9a5a26f18580c7b24632bc9
thundershark <thundershark@yandex.ru>
2014-03-27 19:01:48 +0400

    авторизация по логину паролю (приложение прикидывается браузером) написан класс для парсинга ответов от vk написан простенький интерпритатор api vk — запрос api vk — ответ. Логин, пароль, app id, secret key app беруться из файла, что бы не выкладывать их в паблик ( файл с паролями не индексируется СКВ) починил авторизацию

--- vk-analytic.py
+++ vk-analytic.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'salamander'
 
-import vk_auth
 import vkontakte
 from handlers import reader
 
@@ -11,7 +10,10 @@ def getCredent(file):
     @type param: file
     '''
     f = open(file,'r')
-    return [lines.strip() for lines in f]
+    line =  f.readline().strip()
+    f.close()
+    return line
+
 def razor(req):
     '''
     @rtype: dict
@@ -20,18 +22,20 @@ def razor(req):
 
 
 def main():
-    login, pas, appid, appkey = getCredent('credentials.txt')
+    #vk = vkontakte.API('4264030', 'TMqwtjQP3D1YXMlKmBva')
+    #print (vk.getServerTime())
 
-    (token,user_id) = vk_auth.auth(login, pas, appid, appkey)
-    vk = vkontakte.API(token=token)
+    vk = vkontakte.API(token=getCredent('credentials.txt'))
     #print "Hello vk API , server time is ",vk.getServerTime()
     #print unicode(vk.users.get(uids=146040808))
-    reader.read(vk.users.get(uids=233945283,fields='sex'))
-    print 'input you method'
+    #reader.read(vk.users.get(uids=233945283,fields='sex'))
+    print (vk.getServerTime())
+    print(vk.friends.get(fields='uid, first_name, last_name, nickname, sex, bdate',uid='21229916'))
+    print ('input you method')
     while True:
-        x = raw_input()
+        x = input()
         x = 'vk.%s'%x
-
+        print (x)
         x = eval(x)
         reader.read(x)
     return 0
@@ -41,5 +45,5 @@ if __name__ == '__main__':
         try:
             main()
         except (SyntaxError, RuntimeError) as s:
-            print s
+            print (s)
             continue
