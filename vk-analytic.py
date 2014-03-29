ce3343bb039b5237ab70e513a16e9c0b8c6ca498
thundershark <EkyasJuvJaheotBejyeg>
2014-03-29 23:32:01 +0400

    протестил api друзья id с нужными полями. Ошибка при исполнении в ООП стиле

--- vk-analytic.py
+++ vk-analytic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __author__ = 'salamander'
-
+#взять средие(лушче медиану) от друзей пользователя по городу, возрасту и ВУЗу
 import vkontakte
 from pprint import pprint
 from os.path import exists, isfile
@@ -22,23 +22,35 @@ def getCredent(file):
 
 
 class anyk(object):
-    allUserFields='sex, bdate, city, country, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, online_mobile, lists, domain, has_mobile, contacts, connections, site, education, universities, schools, can_post, can_see_all_posts, can_see_audio, can_write_private_message, status, last_seen, common_count, relation, relatives, counters'
+    __allUserFields='sex, bdate, city, country, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, online_mobile, lists, domain, has_mobile, contacts, connections, site, education, universities, schools, can_post, can_see_all_posts, can_see_audio, can_write_private_message, status, last_seen, common_count, relation, relatives, counters'
+    __kitUserFields='sex, bdate, city, country, online, lists, domain, contacts, connections, site, education, universities, schools, can_post, can_see_all_posts, can_see_audio, can_write_private_message, status, last_seen, common_count, relation, relatives, counters'
+    __researchFields='bdate, city, education, nickname, universities'
+    __researchCmd = "friends.get(user_id=78340794,order='name', fields='bdate, city, education, nickname, universities')"
     t1 = 'friends.getMutual(source_uid=78340794, target_uid=11538362)'
     def __init__(self,tok):
         self.vk=vkontakte.API(token=tok)
     def getMutal(self,id1, id2):
         res = self.vk.getMutual(source_uid=id1, target_uid=id2)
         return res
-    def usersGet(self,ids):
+    def usersGet(self,ids, kitFields=__kitUserFields):
+        """
+        получение информации о некотором человеке
+        """
         if isinstance(ids,list):
             ids=str(ids)[1:-1]
         info = []
         print(ids)
-        print(self.allUserFields)
-        return self.vk.users.get(user_ids=ids, fields=self.allUserFields)
+        print(self.__allUserFields)
+        return self.vk.users.get(user_ids=ids, fields=kitFields)
 
     def eval(self,cmd):
+        """
+        выполняет произвольную команду к api vk
+        """
         return eval('self.vk.%s'%cmd)
+    def medianResearch(self, id):
+        self.eval("friends.get(user_id=%s,order='name', fields='%s')"%(str(id),self.__researchFields))
+
 
 
 
@@ -56,7 +68,8 @@ def main():
     #print "Hello vk API , server time is ",vk.getServerTime()
     #print unicode(vk.users.get(uids=146040808))
     #reader.read(vk.users.get(uids=233945283,fields='sex'))
-    log.responseLog(vk.usersGet(vk.eval(vk.t1)))
+    #log.responseLog(vk.usersGet(vk.eval(vk.t1)))
+    log.responseLog(vk.medianResearch(78340794))
     print ('input you method')
     while True:
         x = input()
