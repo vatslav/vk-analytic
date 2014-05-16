26e30245bde5344b37cf6bb0b9ec4e2729c523cd
Vatslav D <EkyasJuvJaheotBejyeg>
2014-05-16 15:23:17 +0400

    add all fiels web-app2

---
+++ web-app1/app/forms.py
@@ -0,0 +1,8 @@
+__author__ = 'django'
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import  Required
+
+class LoginForm(Form):
+    openid = TextField('openid',validators=[Required()])
+    remember_me = BooleanField('remember_me',default=False)
---
+++ web-app1/app/views.py
@@ -0,0 +1,49 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#    user = { 'nickname': 'Miguel' }
+#    posts = [
+#        {
+#            'author': { 'nickname': 'John' },
+#            'body': 'Beautiful day in Portland!'
+#        },
+#        {
+#            'author': { 'nickname': 'Susan' },
+#            'body': 'The Avengers movie was so cool!'
+#        }
+#    ]
+#    return render_template("index.html",
+#        title = 'Home',
+#        user = user,
+#        posts = posts)
+
+@app.route('/index',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
+def main():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requeted for OpenId= %s , remember_me = %s '%( \
+            form.openid.data,str(form.remember_me.data)))
+        return redirect('/index')
+
+    return  render_template('main.html',
+                            title='Autorition',
+                            form = form,
+                            providers = app.config['OPENID_PROVIDERS'])
+
+@app.route('/login/',methods=['GET','POST'])
+def loginn():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requeted for OpenId= %s , remember_me = %s '%( \
+            form.openid.data,str(form.remember_me.data)))
+        return redirect('/index')
+
+    return  render_template('login.html',
+                            title='Sign In',
+                            form = form,
+                            providers = app.config['OPENID_PROVIDERS'])
